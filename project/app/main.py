02ac49468447b94a5c8e7a1771c534cd3c269108
ItachiEU <komorowski.filip@gmail.com>
2023-03-04 16:04:05 -0500

    Add db and routes

--- project/app/main.py
+++ project/app/main.py
@@ -1,15 +1,33 @@
-from fastapi import FastAPI, Depends
+import logging
 
-from app.config import get_settings, Settings
-
-
-app = FastAPI()
+from fastapi import FastAPI
 
 
-@app.get("/ping")
-async def pong(settings: Settings = Depends(get_settings)):
-    return {
-        "ping": "pong!",
-        "environment": settings.environment,
-        "testing": settings.testing
-    }
+from app.db import init_db
+from app.api import ping, summaries
+
+
+log = logging.getLogger("uvicorn")
+
+
+def create_application() -> FastAPI:
+    application = FastAPI()
+    application.include_router(ping.router)
+    application.include_router(
+        summaries.router, prefix="/summaries", tags=["summaries"])
+
+    return application
+
+
+app = create_application()
+
+
+@app.on_event("startup")
+async def startup_event():
+    log.info("Starting up...")
+    init_db(app)
+
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    log.info("Shutting down...")
